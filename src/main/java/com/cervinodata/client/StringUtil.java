574c0b7c69dbe8b7be46296a10afff0a84a2017a
jaccodrenth <jacco@cervinodata.com>
2023-11-17 16:50:36 +0100

    automated update

--- src/main/java/com/cervinodata/client/StringUtil.java
+++ src/main/java/com/cervinodata/client/StringUtil.java
@@ -16,7 +16,7 @@ package com.cervinodata.client;
 import java.util.Collection;
 import java.util.Iterator;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T15:48:37.146754Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T15:50:30.386542Z[Etc/UTC]")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
