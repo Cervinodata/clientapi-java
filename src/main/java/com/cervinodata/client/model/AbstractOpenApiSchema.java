23306841bafed98b89f3010ad34704222eb37612
jaccodrenth <jacco@cervinodata.com>
2023-11-17 16:14:01 +0100

    automated update

--- src/main/java/com/cervinodata/client/model/AbstractOpenApiSchema.java
+++ src/main/java/com/cervinodata/client/model/AbstractOpenApiSchema.java
@@ -21,7 +21,7 @@ import java.util.Map;
 /**
  * Abstract class for oneOf,anyOf schemas defined in OpenAPI spec
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T15:07:11.116020Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T15:13:52.235529Z[Etc/UTC]")
 public abstract class AbstractOpenApiSchema {
 
     // store the actual instance of the schema/object
