629056b30bd4f45e6edc43b2370a49e44319ee50
jaccodrenth <jacco@cervinodata.com>
2023-11-17 12:55:41 +0100

    automated update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Cervinodata API documentation
 - API version: 1.0.0
-  - Build date: 2023-11-17T11:46:40.416647Z[Etc/UTC]
+  - Build date: 2023-11-17T11:53:33.194501Z[Etc/UTC]
 
 <div style='margin: 2em 0;'>
 <p>
--- src/main/java/com/cervinodata/client/auth/HttpBearerAuth.java
+++ src/main/java/com/cervinodata/client/auth/HttpBearerAuth.java
@@ -20,7 +20,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:46:40.416647Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:53:33.194501Z[Etc/UTC]")
 public class HttpBearerAuth implements Authentication {
   private final String scheme;
   private String bearerToken;
