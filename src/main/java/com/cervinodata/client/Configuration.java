c05f4963abe2554a8b4c67a3afa15ebd2c7a4318
jaccodrenth <jacco@cervinodata.com>
2023-11-17 17:39:06 +0100

    automated update

--- src/main/java/com/cervinodata/client/Configuration.java
+++ src/main/java/com/cervinodata/client/Configuration.java
@@ -13,7 +13,7 @@
 
 package com.cervinodata.client;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T16:20:33.470182Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T16:38:51.622112Z[Etc/UTC]")
 public class Configuration {
     public static final String VERSION = "0.1.0";
 
