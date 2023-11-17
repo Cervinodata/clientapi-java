f00e713f01be29a752eae34955b3a999421be4e5
jaccodrenth <jacco@cervinodata.com>
2023-11-17 12:43:41 +0100

    automated update

--- src/main/java/com/cervinodata/client/Configuration.java
+++ src/main/java/com/cervinodata/client/Configuration.java
@@ -13,7 +13,7 @@
 
 package com.cervinodata.client;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:16:54.634301Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:43:28.344364Z[Etc/UTC]")
 public class Configuration {
     public static final String VERSION = "0.1.0";
 
--- src/main/java/com/cervinodata/client/StringUtil.java
+++ src/main/java/com/cervinodata/client/StringUtil.java
@@ -16,7 +16,7 @@ package com.cervinodata.client;
 import java.util.Collection;
 import java.util.Iterator;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:16:54.634301Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T11:43:28.344364Z[Etc/UTC]")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
