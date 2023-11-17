a10accfab929f8be3ef0505758eb558857b36962
jaccodrenth <jacco@cervinodata.com>
2023-11-17 18:20:23 +0100

    automated update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Cervinodata API documentation
 - API version: 1.0.0
-  - Build date: 2023-11-17T17:14:49.403319Z[Etc/UTC]
+  - Build date: 2023-11-17T17:20:13.534817Z[Etc/UTC]
 
 <div style='margin: 2em 0;'>
 <p>
--- src/main/java/com/cervinodata/client/auth/ApiKeyAuth.java
+++ src/main/java/com/cervinodata/client/auth/ApiKeyAuth.java
@@ -20,7 +20,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.List;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T17:14:49.403319Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T17:20:13.534817Z[Etc/UTC]")
 public class ApiKeyAuth implements Authentication {
   private final String location;
   private final String paramName;
